ec08c3dbe19b69c147743764833c193fd0055eea
Justin Lu <justinlu@vt.edu>
2015-12-07 21:47:41 -0500

    Added code structure

--- main.py
+++ main.py
@@ -2,16 +2,52 @@
 # Modified: 12/11/15
 # Authors: Justin Lu and Richard Ma
 # Description: Main file for blackjack game
-#http://stackoverflow.com/questions/2518753/best-way-to-implement-a-deck-for-a-card-game-in-python
+# http://stackoverflow.com/questions/2518753/best-way-to-implement-a-deck-for-a-card-game-in-python
 import random
+import sys
+import os
 import itertools
 #SUITS = 'cdhs'
 RANKS = '23456789TJQKA'
 DECK = tuple(''.join(card) for card in itertools.product(RANKS))
 
-hand = random.sample(DECK, 5)
-print hand
-#just testing
-print hand.pop()
-print hand
+hand_player = random.sample(DECK, 2)
+hand_dealer = random.sample(DECK, 2)
+print (hand_player)
+print (hand_dealer)
+
 #need to remove cards from deck to ensure no duplicates if we're playing multiple rounds
+
+#define Hand class
+class Hand:
+    def __init__(self):
+        pass #pass statement indicates code needs to go here
+    def __str__(self):
+        pass
+    def add(self):
+        pass
+    def getVal(self):
+        pass
+
+class Deck:
+    def __init__(self):
+        pass
+    def __str__(self):
+        pass
+    def deal(self):
+        pass
+    def shuffle(self):
+        pass
+#Functions for player actions
+
+def deal():
+    pass
+
+def hit():
+    pass
+
+def stand():
+    pass
+
+def draw():
+    pass
