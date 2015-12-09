780ec23092ac8a48f6078b14a92c6aa30bfbbf7a
justinlu <justinlu@vt.edu>
2015-12-09 15:36:35 -0500

    Added what happens at each of the dealer and player turns

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Authors: Justin Lu and Richard Ma
 # Description: Main file for blackjack game
 # http://stackoverflow.com/questions/2518753/best-way-to-implement-a-deck-for-a-card-game-in-python
+# Rules of Blackjack found at:
+# http://www.pagat.com/banking/blackjack.html#player
 import random
 import sys
 import os
@@ -10,53 +12,99 @@ import itertools
 RANKS = '23456789TJQKA'
 DECK = tuple(''.join(card) for card in itertools.product(RANKS))
 
-hand_player = random.sample(DECK, 2)
-hand_dealer = random.sample(DECK, 2)
-print "Player"
-print (hand_player)
-hand_player.append(random.sample(DECK, 1)[0])
-print (hand_player)
-
-print "Dealer"
-print (hand_dealer)
-print (hand_dealer[1])
-dealerValue=0
-for index in range(len(hand_dealer)):
-    if(hand_dealer[index]=='T'):
-        dealerValue+=10
-    elif(hand_dealer[index]=='J'):
-        dealerValue+=10
-    elif(hand_dealer[index]=='K'):
-        dealerValue+=10
-    elif(hand_dealer[index]=='Q'):
-        dealerValue+=10
-    elif(hand_dealer[index]=='A'):
-        dealerValue+=1
-    else:
-        dealerValue += int(hand_dealer[index])
-print dealerValue
-
-
 #need to remove cards from deck to ensure no duplicates if we're playing multiple rounds
 
 
 #Functions for player actions
 
-def deal():
-    pass
 
 def hit(hand):
     #add a new card to the hand
     newCard = random.sample(DECK, 1)
-    hand.append(newCard)
+    hand.append(newCard[0])
     print hand
 
 def bust (player):
+    #if player hand value is over 21, they lose
     print 'Bust!'
     print player + 'loses!'
+
+def getHandValue(hand):
+    value = 0
+    for index in range(len(hand)):
+        if(hand[index]=='T'):
+            value+=10
+        elif(hand[index]=='J'):
+            value+=10
+        elif(hand[index]=='K'):
+            value+=10
+        elif(hand[index]=='Q'):
+            value+=10
+        elif(hand[index]=='A'):
+            value+=1
+        else:
+            value += int(hand[index])
+
+    return value
 
 def stand():
     pass
 
 def newBlackjackGame():
-    pass
+
+    bust = False
+    playerTurnOver = False
+    dealerTurnOver = False
+    dealerValue=0
+    playerValue=0
+    hand_player = random.sample(DECK, 2)
+    hand_dealer = random.sample(DECK, 2)
+    print "Player"
+    print (hand_player)
+    playerValue = getHandValue(hand_player)
+    print playerValue
+
+    print "Dealer"
+    print (hand_dealer)
+    print (hand_dealer[1])
+    dealerValue = getHandValue(hand_dealer)
+
+    # PLAYER'S TURN
+
+    while (playerTurnOver == False):
+         action = raw_input ('PLAYERS TURN: Would you like to hit or stand? Type "hit" or "stand" ')
+         if (action == 'hit'):
+            hit (hand_player)
+         elif (action == 'stand'):
+            playerTurnOver = True
+         else:
+            print 'INVALID ACTION'
+    # DEALER'S TURN
+    while (dealerTurnOver == False):
+        dealerValue = getHandValue (hand_dealer)
+        if (dealerValue < 17): # dealer must hit if hand value is lower than 17
+            hit(hand_dealer)
+            dealerValue = getHandValue(hand_dealer)
+            if (dealerValue > 21):
+                bust('Dealer')
+                break
+            else:
+                dealerTurnOver = True
+        elif (dealerValue == 21): # dealer has blackjack
+            dealerTurnOver = True
+        else:
+            dealerTurnOver = True
+
+    dealerValue = getHandValue(hand_dealer)
+    playerValue = getHandValue(hand_player)
+
+    print 'Value of Player Hand: ' + str(playerValue)
+    print 'Value of Dealer Hand: ' + str(dealerValue)
+
+    if (playerValue > dealerValue):
+        print 'Player wins!'
+
+    else:
+        print 'House wins!'
+
+newBlackjackGame()
